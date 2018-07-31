63179201a05f53cb3c8e1cd794ede1d16d8a6ed3
jeremykarst2007 <jeremykarst2007@gmail.com>
2018-07-31 12:35:03 -0400

    Added wavelet transform limit and efficiency improvements.
    Should give a dramatic speedup from original code due to less intermediate results, more efficient data structures, and elimination of the random number generation.

--- ampdLib.py
+++ ampdLib.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__      = "Luca Cerina"
 __copyright__   = "Copyright 2017, Luca Cerina"
+__credits__     = ["Jeremy Karst"]
 __email__       = "luca.cerina@polimi.it"
 
 
 import numpy as np
 
 # AMPD function
-def ampd(sigInput):
+def ampd(sigInput, LSMlimit = 1):
 	"""Find the peaks in the signal with the AMPD algorithm.
 	
 		Original implementation by Felix Scholkmann et al. in
@@ -19,6 +20,12 @@ def ampd(sigInput):
 		----------
 		sigInput: ndarray
 			The 1D signal given as input to the algorithm
+		lsmLimit: float
+			Wavelet transform limit as a ratio of full signal length.
+			Valid values: 0-1, the LSM array will no longer be calculated after this point
+			  which results in the inability to find peaks at a scale larger than this factor.
+			  For example a value of .5 will be unable to find peaks that are of period 
+			  1/2 * signal length, a default value of 1 will search all LSM sizes.
 
 		Returns
 		-------
@@ -28,41 +35,31 @@ def ampd(sigInput):
 		
 	# Create preprocessing linear fit	
 	sigTime = np.arange(0, len(sigInput))
-	
 	fitPoly = np.polyfit(sigTime, sigInput, 1)
-	sigFit = np.polyval(fitPoly, sigTime)
 	
 	# Detrend
-	dtrSignal = sigInput - sigFit
+	dtrSignal = (sigInput - np.polyval(fitPoly, sigTime)).astype(float)
 	
 	N = len(dtrSignal)
-	L = int(np.ceil(N / 2.0)) - 1
+	L = int(np.ceil(N*LSMlimit / 2.0)) - 1
 	
 	# Generate random matrix
-	LSM = np.random.random([L,N])+1 # uniform + alpha = 1
+	LSM = np.ones([L,N], dtype='uint8')
 	
 	# Local minima extraction
 	for k in np.arange(1, L):
-		locMax = np.zeros(N, dtype=bool)
-		mask = (dtrSignal[k:N - k - 1] > dtrSignal[0: N - 2 * k - 1]) & (dtrSignal[k:N - k - 1] > dtrSignal[2 * k: N - 1])
-		mask = mask.flatten()
-
-		locMax[k:N-k-1] = mask
-		LSM[k - 1, locMax] = 0
+		LSM[k - 1, np.where((dtrSignal[k:N - k - 1] > dtrSignal[0: N - 2 * k - 1]) & (dtrSignal[k:N - k - 1] > dtrSignal[2 * k: N - 1]))[0]+k] = 0
 	
 	# Find minima				
 	G = np.sum(LSM, 1)
 	l = np.where(G == G.min())[0][0]
-	
-	LSM = LSM[0:l, :]
-	
-	S = np.sum(LSM, 0)
 
-	pks = np.flatnonzero(S == 0)
+	pks = np.where(np.sum(LSM[0:l, :], 0)==0)[0]
 	return pks
 
+
 # Fast AMPD		
-def ampdFast(sigInput, order):
+def ampdFast(sigInput, order, LSMlimit = 1):
 	"""A slightly faster version of AMPD which divides the signal in 'order' windows
 
 		Parameters
@@ -90,7 +87,7 @@ def ampdFast(sigInput, order):
 	# Loop function calls
 	for i in range(0, len(sigInput)-N, N):
 		print("\t sector: " + str(i) + "|" + str((i+2*N-1)))
-		pksTemp = ampd(sigInput[i:(i+2*N-1)])
+		pksTemp = ampd(sigInput[i:(i+2*N-1)], LSMlimit)
 		if(i == 0):
 			pks = pksTemp
 		else:
@@ -100,4 +97,3 @@ def ampdFast(sigInput, order):
 	pks = np.unique(pks)
 	
 	return pks
-		
